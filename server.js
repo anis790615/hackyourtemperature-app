fe72de451f12a53c01783de5ff573706f1463c04
Anis Alkomem <anis.alkumaim@gmail.com>
2020-06-02 12:39:12 +0200

    Fixed an error with setting the port

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.use(express.static("public"));
 // Use routes
 app.use(weatherRoutes);
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
